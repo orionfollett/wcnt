ba47cf3974a34c73dfc3d1ad9e3c12ae6f74c31b
Orion Follett <orion.follett@firstresonance.io>
2025-11-26 23:00:32 -0800

    arena alloc

--- main.zig
+++ main.zig
@@ -1,14 +1,17 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const allocator = std.heap.raw_c_allocator;
+    var aa = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer _ = aa.deinit();
+    var a = aa.allocator();
+
     const file_name = "README.md";
 
     const f = try std.fs.cwd().openFile(file_name, .{});
     defer f.close();
 
-    const s = try f.readToEndAlloc(allocator, 1000000000);
-    defer allocator.free(s);
+    const s = try f.readToEndAlloc(a, 1000000000);
+    defer a.free(s);
 
     std.debug.print("{s}", .{s});
 }
