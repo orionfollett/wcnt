23470382e95ed4e140c307babd549beb3d084acd
Orion Follett <orion.follett@firstresonance.io>
2025-11-27 12:01:07 -0800

    first step

--- main.zig
+++ main.zig
@@ -3,16 +3,25 @@ const std = @import("std");
 pub fn main() !void {
     var aa = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer _ = aa.deinit();
-    var a = aa.allocator();
+    const a = aa.allocator();
 
     const file_name = "README.md";
-
     const f = try std.fs.cwd().openFile(file_name, .{});
     defer f.close();
     const s = try f.readToEndAlloc(a, 1000000000);
-    defer a.free(s);
-    var it = std.mem.tokenizeAny(u8, s, "!@#$%^&*()-+ /,|{}[]\n\t=`\"\'");
+
+    var word_counts = std.hash_map.StringHashMap(u32).init(a);
+    var it = std.mem.tokenizeAny(u8, s, ".,;:!@#$%^&*()-+ /|{}[]\n\t=`\"\'");
     while (it.next()) |token| {
-        std.debug.print(" {s} ", .{token});
+        const result = try word_counts.getOrPut(token);
+        if (!result.found_existing) {
+            result.value_ptr.* = 1;
+        } else {
+            result.value_ptr.* += 1;
+        }
+    }
+    var nit = word_counts.iterator();
+    while (nit.next()) |item| {
+        std.debug.print("{s} {d}\n", .{ item.key_ptr.*, item.value_ptr.* });
     }
 }
