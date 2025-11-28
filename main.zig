9c42abaaa7d0af31f9d11f6eb627fe4e3a69ad8e
Orion Follett <orion.follett@firstresonance.io>
2025-11-27 23:02:45 -0800

    List top 5

--- main.zig
+++ main.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
 
+const WordCount = struct {
+    count: u32,
+    word: []const u8,
+};
+fn WordCountLessThan(_: void, lhs: WordCount, rhs: WordCount) bool {
+    return lhs.count < rhs.count;
+}
+
 pub fn main() !void {
     var aa = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer _ = aa.deinit();
@@ -20,8 +28,25 @@ pub fn main() !void {
             result.value_ptr.* += 1;
         }
     }
+
+    var word_count_list = try std.array_list.Aligned(WordCount, null).initCapacity(a, word_counts.capacity() * @sizeOf(WordCount));
     var nit = word_counts.iterator();
     while (nit.next()) |item| {
-        std.debug.print("{s} {d}\n", .{ item.key_ptr.*, item.value_ptr.* });
+        try word_count_list.append(a, .{ .count = item.value_ptr.*, .word = item.key_ptr.* });
+    }
+
+    std.sort.pdq(
+        WordCount,
+        word_count_list.items,
+        {},
+        WordCountLessThan,
+    );
+
+    const end = @max(0, word_count_list.items.len - 10);
+    var start = word_count_list.items.len - 1;
+    while (start >= end) {
+        const i = word_count_list.items[start];
+        std.debug.print("{s} {d}\n", .{ i.word, i.count });
+        start -= 1;
     }
 }
